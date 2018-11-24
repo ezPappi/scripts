bdfde618522f0b7ee8a46d53c64c6c45ab13a20e
Marcin M. Jessa <ezunix@users.noreply.github.com>
2018-11-24 18:54:56 +0100

    Create osx_to_iso.sh

---
+++ shell/osx_to_iso.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Mac OSX ISO creator
+# First download OSX from the App Store
+
+hdiutil create -o /tmp/Mojave.cdr -size 6g -layout SPUD -fs HFS+J
+hdiutil attach /tmp/Mojave.cdr.dmg -noverify -mountpoint /Volumes/install_build
+
+sudo /Applications/Install\ macOS\ Mojave.app/Contents/Resources/createinstallmedia --volume /Volumes/install_build
+mv /tmp/Mojave.cdr.dmg ~/Mojave.cdr.dmg 
+hdiutil detach /Volumes/Install\ macOS\ Mojave
+    
+hdiutil convert ~/Mojave.cdr.dmg -format UDTO -o ~/Desktop/Mojave.iso
+mv ~/Desktop/Mojave.iso.cdr ~/Desktop/Mojave.iso
+rm ~/Mojave.cdr.dmg
