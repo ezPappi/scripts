9529f9bbf5ea6a3ba79c9e5b15a7b4aaa4fc941c
Marcin M. Jessa <ezunix@users.noreply.github.com>
2019-04-10 14:46:26 +0200

    Update persistant_azure_login.ps1

--- powershell/persistant_azure_login.ps1
+++ powershell/persistant_azure_login.ps1
@@ -8,13 +8,13 @@ Get-Module -ListAvailable Az.* | Select Name, Version
 Connect-AzAccount
 
 # Reuse Azure Authentication in order to avoid the steps of re-opening the browser.
-Save-AzContext
+Save-AzContext -Path C:\path\to\\AzureAuthfile.json
 
 # After exiting your PowerShell session, in order to reconnect again load authentication information from a file.
-Import-AzContext
+Import-AzContext -Path C:\path\to\\AzureAuthfile.json
 
 # Allow the azure credential, account and subscription information to be saved and automatically loaded when you open a PowerShell window.
 Enable-AzContextAutoSave
 
 # Or don't.
-Disable- AzContextAutoSave
+Disable-AzContextAutoSave
